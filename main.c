9828f324fe08915661c0d9f2db580ef3340cb30b
AntWANLAN <afevre@myges.fr>
2020-01-30 21:19:35 +0100

    300120 21:29

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 typedef struct Pixel_t {
     uint8_t red;
@@ -15,6 +18,14 @@ typedef struct Ppm_image_t {
     size_t length;
     Pixel_t * pixelArray;
 } Ppm_image_t;
+
+uint64_t get_file_size(const uint8_t * filePath) {
+    struct stat fileInfos;
+
+    fstat(open((char *)filePath, O_RDONLY), &fileInfos);
+
+    return fileInfos.st_size;
+}
 
 Pixel_t pixel_new(uint8_t red, uint8_t green, uint8_t blue) {
     Pixel_t newPixel;
@@ -54,12 +65,35 @@ Pixel_t pixel_invert(const Pixel_t * p) {
     return invertedPixel;
 }
 
-Ppm_image_t ppm_new(const char * pathname) {
-    FILE * picture = fopen(pathname, "r+b");
+Ppm_image_t ppm_new(const uint8_t * pathname) {
+    FILE * picture = fopen((char *)pathname, "r+b");
+    Ppm_image_t imageInfos;
+    uint64_t fileSize;
+    uint8_t * line;
 
+    fileSize = get_file_size(pathname);
+    line = malloc(sizeof(uint8_t) * fileSize);
+    if(line != NULL) {
+        fgets((char *)line, fileSize, picture);
+        fgets((char *)line, fileSize, picture);
+    } else {
+        fprintf(stderr, "Can't malloc line in 'ppm_new'");
+        exit(EXIT_FAILURE);
+    }
+    printf("%s\n", line);
+    imageInfos.length = fileSize;
+    imageInfos.height = 8;
+    imageInfos.width = 8;
+    imageInfos.pixelArray = malloc(sizeof(Pixel_t) * fileSize);
     
+    return imageInfos;
 }
 
 int main(void) {
+    Ppm_image_t image;
+    image = ppm_new((uint8_t *)"image.ppm");
 
+    printf("%ld", image.height);
+
+    return EXIT_SUCCESS;
 }
