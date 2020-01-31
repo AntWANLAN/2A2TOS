92cb076e67409a6767e3e8972ee01f424fe107b5
AntWANLAN <afevre@myges.fr>
2020-01-31 13:02:36 +0100

    Update 310120 13:02

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <ctype.h>
 
 typedef struct Pixel_t {
     uint8_t red;
@@ -67,33 +68,66 @@ Pixel_t pixel_invert(const Pixel_t * p) {
 
 Ppm_image_t ppm_new(const uint8_t * pathname) {
     FILE * picture = fopen((char *)pathname, "r+b");
-    Ppm_image_t imageInfos;
+    Ppm_image_t imageInfos = {0, 0, 0, NULL};
     uint64_t fileSize;
     uint8_t * line;
+    uint16_t i;
 
     fileSize = get_file_size(pathname);
     line = malloc(sizeof(uint8_t) * fileSize);
     if(line != NULL) {
-        fgets((char *)line, fileSize, picture);
-        fgets((char *)line, fileSize, picture);
+        imageInfos.length = fileSize;
+
+        fgets((char *)line, fileSize, picture); //image type
+        fgets((char *)line, fileSize, picture); //image width and height
+        while(isspace(line[i]))i++;
+        imageInfos.width = atoi((char *)&line[i++]);
+
+        while(isdigit(line[i]))i++;
+        while(isspace(line[i]))i++;
+        imageInfos.height = atoi((char *)&line[i++]);
+
+        imageInfos.pixelArray = malloc(sizeof(Pixel_t) * (fileSize / 3));
     } else {
         fprintf(stderr, "Can't malloc line in 'ppm_new'");
         exit(EXIT_FAILURE);
     }
-    printf("%s\n", line);
-    imageInfos.length = fileSize;
-    imageInfos.height = 8;
-    imageInfos.width = 8;
-    imageInfos.pixelArray = malloc(sizeof(Pixel_t) * fileSize);
+    
     
     return imageInfos;
 }
 
 int main(void) {
     Ppm_image_t image;
-    image = ppm_new((uint8_t *)"image.ppm");
+    image = ppm_new((uint8_t *)"Aerial.512.ppm");
 
-    printf("%ld", image.height);
+    printf("%ld x %ld\n", image.width, image.height);
+
+    // for(int i = 0; i < 6; i++) {
+    //     printf("%c %c %c\n",
+    //         image.pixelArray[i].red,
+    //         image.pixelArray[i].green,
+    //         image.pixelArray[i].blue
+    //     );
+    // }
+    
+    /* question 11
+    imageInfos.pixelArray = malloc(sizeof(Pixel_t) * (fileSize / 3));
+    fgets((char *)line, fileSize, picture); //maximum value for each color
+    fgets((char *)line, fileSize, picture);
+    while(line[0] == '#') {
+        fgets((char *)line, fileSize, picture);
+        printf("%s", (char *)line);
+    }
+    for(i = 0; i < sizeof(Pixel_t) * (fileSize / 3); i++) {
+            sscanf((char *)line, "%u %u %u", &red, &green, &blue);
+            pixel.red = red;
+            pixel.green = green;
+            pixel.blue = blue;
+            imageInfos.pixelArray[i] = pixel;
+            fgets((char *)line, fileSize, picture);
+    }
+    */
 
     return EXIT_SUCCESS;
 }
