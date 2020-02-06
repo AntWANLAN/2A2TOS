9438e7962e8a9c96cdd2daf5962e357b64262561
AntWANLAN <afevre@myges.fr>
2020-02-06 18:02:05 +0100

    stable 060220 18:01

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
 #include <stdbool.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -66,8 +67,24 @@ Pixel_t pixel_invert(const Pixel_t * p) {
     return invertedPixel;
 }
 
+int ppm_get_height(const Ppm_image_t * image){
+    return image->height;
+}
+
+size_t ppm_get_width(const Ppm_image_t * image){
+    return image->width;
+}
+
+size_t ppm_get_lenght(const Ppm_image_t * image){
+    return image->length;
+}
+
+Pixel_t ppm_pixel(const Ppm_image_t * img, const size_t x, const size_t y) {
+    return img->pixelArray[y + x * img->width];
+}
+
 Ppm_image_t ppm_new(const uint8_t * pathname) {
-    FILE * picture = fopen((char *)pathname, "r+b");
+    FILE * picture = fopen((char *)pathname, "rb");
     Ppm_image_t imageInfos = {0, 0, 0, NULL};
     uint64_t fileSize;
     uint8_t * line;
@@ -87,47 +104,76 @@ Ppm_image_t ppm_new(const uint8_t * pathname) {
         while(isspace(line[i]))i++;
         imageInfos.height = atoi((char *)&line[i++]);
 
-        imageInfos.pixelArray = malloc(sizeof(Pixel_t) * (fileSize / 3));
+        imageInfos.pixelArray = malloc(
+                sizeof(Pixel_t) * imageInfos.width * imageInfos.height
+            );
     } else {
         fprintf(stderr, "Can't malloc line in 'ppm_new'");
         exit(EXIT_FAILURE);
     }
     
+    fclose(picture);
     
     return imageInfos;
 }
 
+void printPixel(const Pixel_t * pixel){
+    printf("R: %d\tG: %d\tB: %d\n", pixel->red, pixel->green, pixel->blue);
+}
+
+void ppm_negatif(Ppm_image_t * img) {
+    uint32_t x;
+    uint32_t y;
+    uint32_t i = 0;
+    uint8_t fileName[256];
+    FILE * file = NULL;
+    Pixel_t pixel;
+
+    for(x = 0; x < ppm_get_height(img); x++) {
+        for(y = 0; y < ppm_get_width(img); y++) {
+            pixel = ppm_pixel(img, x, y);
+            img->pixelArray[i] = pixel_invert(&pixel);
+            i++;
+        }
+    }
+}
+
+void loadImage(const uint8_t * pathname, Ppm_image_t * img) {
+    FILE * picture;
+    Pixel_t pixel;
+    uint16_t i, j = 0;
+    uint64_t size;
+    uint8_t * buffer;
+
+    picture = fopen((char *)pathname, "rb");
+
+    size = img->width * 3;
+    buffer = malloc(sizeof(uint8_t) * (size + 1));
+    buffer[size] = '\0';
+
+    fgets((char *)buffer, size, picture); //image type
+    fgets((char *)buffer, size, picture); //image width and height
+    fgets((char *)buffer, size, picture); //maximum value for each color
+
+    for(i = 0; fread(buffer, sizeof(uint8_t), 3, picture), i < img->height * img->width && !feof(picture); i += 3) {
+        pixel = pixel_new(
+            (uint8_t)buffer[0],
+            (uint8_t)buffer[1],
+            (uint8_t)buffer[2]
+        );
+        img->pixelArray[j++] = pixel;
+        printPixel(&pixel);
+    }
+    free(buffer);
+    fclose(picture);
+}
+
 int main(void) {
-    Ppm_image_t image;
-    image = ppm_new((uint8_t *)"Aerial.512.ppm");
+    Ppm_image_t img;
+    img = ppm_new((uint8_t *)"Aerial.512.ppm");
 
-    printf("%ld x %ld\n", image.width, image.height);
-
-    // for(int i = 0; i < 6; i++) {
-    //     printf("%c %c %c\n",
-    //         image.pixelArray[i].red,
-    //         image.pixelArray[i].green,
-    //         image.pixelArray[i].blue
-    //     );
-    // }
-    
-    /* question 11
-    imageInfos.pixelArray = malloc(sizeof(Pixel_t) * (fileSize / 3));
-    fgets((char *)line, fileSize, picture); //maximum value for each color
-    fgets((char *)line, fileSize, picture);
-    while(line[0] == '#') {
-        fgets((char *)line, fileSize, picture);
-        printf("%s", (char *)line);
-    }
-    for(i = 0; i < sizeof(Pixel_t) * (fileSize / 3); i++) {
-            sscanf((char *)line, "%u %u %u", &red, &green, &blue);
-            pixel.red = red;
-            pixel.green = green;
-            pixel.blue = blue;
-            imageInfos.pixelArray[i] = pixel;
-            fgets((char *)line, fileSize, picture);
-    }
-    */
+    printf("Image size %ld x %ld\n", img.width, img.height);
+    loadImage((uint8_t *)"Aerial.512.ppm", &img);
 
     return EXIT_SUCCESS;
 }
